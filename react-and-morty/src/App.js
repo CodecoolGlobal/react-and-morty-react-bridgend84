f39132a896930a2482dad4330f2fb6c9aa6ef99b
bridgend84 <sacrun@gmail.com>
2022-09-12 16:05:16 +0200

    Feat: char names displayed

--- react-and-morty/src/App.js
+++ react-and-morty/src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import { useCharacters, useLocations } from "./api/useData";
 
 function App() {
+  const [isCharLoaded, setIsCharLoaded] = useState(false);
+
   const characters = useCharacters(1);
   const locations = useLocations(1);
 
@@ -12,10 +14,16 @@ function App() {
   console.log("Locations data: ");
   console.log(locations);
 
+  useEffect(() => {
+    characters === "Loading..."
+      ? setIsCharLoaded(false)
+      : setIsCharLoaded(true);
+  });
+
   return (
-    <div className="App">
+    <div className="container-app">
       <Header />
-      <h3>Take a look at the console! (F12)</h3>
+      {isCharLoaded ? characters.results.map(char => (<p>{char.name}</p>)) : (<p>Loading...</p>)}
     </div>
   );
 }
--- react-and-morty/src/components/Header.js
+++ react-and-morty/src/components/Header.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Header.css";
+import rNmLogo from "../static/rick-and-morty-logo.png";
 
 export default function Header() {
-    return (
-        <h1>Ez itt a header</h1>
-    )
+  return (
+    <div className="container-header">
+      <img src={rNmLogo} />
+    </div>
+  );
 }
