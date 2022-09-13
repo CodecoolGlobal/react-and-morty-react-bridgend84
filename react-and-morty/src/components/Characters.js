5d92fc38f54901646f068605951e4fc3156c1bf0
bridgend84 <sacrun@gmail.com>
2022-09-13 15:36:57 +0200

    Feat: character display

--- react-and-morty/src/components/Characters.js
+++ react-and-morty/src/components/Characters.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "./Characters.css";
 
-export default function Characters({ characters }) {
+export default function Characters({ characters, pageSelector }) {
   return (
     <div className="Characters">
-      {characters.map((char) => (
-        <p key={char.name}>{char.name}</p>
-      ))}
+      <input
+        type="number"
+        defaultValue={1}
+        min={1}
+        max={characters.info.pages}
+        onChange={(e) => pageSelector(e.target.value)}
+      />
+      <div className="character-container">
+        {characters.results.map((char) => (
+          <div key={char.id} className="character-card">
+            <img src={char.image}></img>
+            <div>{char.name}</div>
+            <div>{char.species}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
