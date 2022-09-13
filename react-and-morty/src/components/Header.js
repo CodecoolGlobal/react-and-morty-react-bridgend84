d70a37672e7b0675d6e7855d53da69afcadeafb3
bridgend84 <sacrun@gmail.com>
2022-09-13 13:24:39 +0200

    Merge

--- react-and-morty/src/components/Header.js
+++ react-and-morty/src/components/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <div className="header">
       <div className="container-header">
-        <img src={rNmLogo} />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Rick_and_Morty.svg" />
       </div>
       <div className="header-buttons">
         <button className="header-characters-button">Characters</button>
