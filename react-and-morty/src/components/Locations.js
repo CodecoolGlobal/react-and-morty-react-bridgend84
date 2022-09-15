8ab36b10ab68366983d41b1cf1840c1f3fb6913b
Marton Benjamin Szak <105061239+martonszak@users.noreply.github.com>
2022-09-15 21:34:48 +0200

    Style: Locations page CSS finished

--- react-and-morty/src/components/Locations.js
+++ react-and-morty/src/components/Locations.js
@@ -6,20 +6,27 @@ import { Flipper, Flipped } from "react-flip-toolkit";
 export default function Locations({ locations, pageSelector }) {
   const [focused, setFocused] = useState(null);
 
-  const onClick = (index) =>{
+  const onClick = (index) => {
     focused === index ? setFocused(null) : setFocused(index);
   };
 
   return (
     <div className="Locations">
-      <input
-        type="number"
-        defaultValue={1}
-        min={1}
-        max={locations.info.pages}
-        onChange={(e) => pageSelector(e.target.value)}
-      />
-       <Flipper
+      <div className="page-number-selector-container">
+        <label for="page-number-loc" className="page-number-selector">
+          Select page
+        </label>
+        <input
+          id="page-number"
+          className="page-number-selector"
+          type="number"
+          defaultValue={1}
+          min={1}
+          max={locations.info.pages}
+          onChange={(e) => pageSelector(e.target.value)}
+        />
+      </div>
+      <Flipper
         flipKey={focused}
         spring="super gentle"
         staggerConfig={{
@@ -30,36 +37,36 @@ export default function Locations({ locations, pageSelector }) {
         decisionData={focused}
         transformOrigin="300 300"
       >
-          <div className="character-container">
-            {locations.results.map((loc) =>
-              focused === loc.id ? (
-                <Flipped flipId={loc.id} key={loc.id}>
-                  <div
-                    key={loc.id}
-                    className="character-card-back"
-                    onClick={() => onClick(loc.id)}
-                  >
-                    <ul className="list-back"></ul>
-                    <li>Name: {loc.name}</li>
-                    <li>Type: {loc.type}</li>
-                    <li>Dimension: {loc.dimension}</li>
-                  </div>
-                </Flipped>
-              ) : (
-                <Flipped flipId={loc.id} key={loc.id}>
-                  <div
+        <div className="location-container">
+          {locations.results.map((loc) =>
+            focused === loc.id ? (
+              <Flipped flipId={loc.id} key={loc.id}>
+                <div
                   key={loc.id}
-                    className="character-card-front"
-                    onClick={() => onClick(loc.id)}
-                  >
-                    <ul className="list-front">
-                    <li className="name-front">{loc.name}</li>
-                    </ul>
-                  </div>
-                </Flipped>
-              )
-            )}
-          </div>
+                  className="location-card-back"
+                  onClick={() => onClick(loc.id)}
+                >
+                  <ul className="list-back"></ul>
+                  <li>Name: {loc.name}</li>
+                  <li>Type: {loc.type}</li>
+                  <li>Dimension: {loc.dimension}</li>
+                </div>
+              </Flipped>
+            ) : (
+              <Flipped flipId={loc.id} key={loc.id}>
+                <div
+                  key={loc.id}
+                  className="location-card-front"
+                  onClick={() => onClick(loc.id)}
+                >
+                  <ul className="loc-list-front">
+                    <li className="loc-name-front">{loc.name}</li>
+                  </ul>
+                </div>
+              </Flipped>
+            )
+          )}
+        </div>
       </Flipper>
     </div>
   );
